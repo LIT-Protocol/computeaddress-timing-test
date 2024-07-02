8487036cccadf8254002fa0d4aa57ccac2d5933f
Chris Cassano <chris@litprotocol.com>
2024-07-01 17:55:00 -0700

    add max heap size vars

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # these are the same V8 flags we use in the Lit node
-deno run -A --v8-flags=--jitless,--no-expose-wasm,--no-use-ic,--disallow-code-generation-from-strings,--clear-free-memory --no-code-cache main.ts
+deno run -A --v8-flags=--jitless,--no-expose-wasm,--no-use-ic,--disallow-code-generation-from-strings,--clear-free-memory,--max-heap-size=256,--max-old-space-size=256 --no-code-cache main.ts
